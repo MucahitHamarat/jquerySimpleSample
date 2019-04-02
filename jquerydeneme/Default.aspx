483df9886a533087dc1dc03b05d893135be2d497
McHt HmRt <mucahithamarat@gmail.com>
2019-04-02 11:00:55 +0300

    Add project files.

---
+++ jquerydeneme/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="jquerydeneme.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <p>Bu bir cümledir</p>
+            <a href="#">Bu bir hepsiburada linkidir</a>
+            <div class="div1" style="visibility:hidden ">Bu bir divdir</div>
+            <input type="button" value="Gizle" onclick="hareket1()" class="b1"   />
+            <input type="button" value="Göster" onclick="hareket2()" class="b2" onmouseover="goster()"/>
+            <input type="button" value="Gizle+Göster" onclick="hareket3()" class="b3"/>
+        </div>
+    </form>  
+    <script src="jquery.js"></script>
+    <script src="main.js"></script>
+</body>
+</html>
---
+++ jquerydeneme/main.js
@@ -0,0 +1,38 @@
+﻿$("p").css({ "color": "red", "text-decoration": "underline" });
+$("a").attr("href", "https://www.hepsiburada.com");
+$(".div1").css({ "background-image": "url('/images/pic01.jpg')", "width": "400px", "height": "250px","background-repeat":"no-repeat" }); 
+$(".b1").css({ "height":"75px","top": "0", "left": "500px" ,"position":"absolute"});
+$(".b2").css({ "height": "75px", "top": "0", "left": "600px", "position": "absolute" });
+$(".b3").css({ "height": "75px", "top": "0", "left": "700px", "position": "absolute" });
+
+
+
+    function hareket1() {
+        //$(".div1").fadeIn(1000);
+        $(".div1").slideUp(2000);
+       
+    }
+    ;
+
+    function hareket2() {
+        //$(".div1").fadeOut(2000);
+        $(".div1").slideDown(1500);
+        $(".div1").css("visibility", "visible");
+    }
+    ;
+    function hareket3() {
+        //$(".div1").fadeToggle(2000);
+        $(".div1").slideToggle(2000);
+    }
+;
+function goster() {
+    $(".div1").css("visibility","visible")
+};
+
+    $(document).ready(function(){
+        $("p").click(function () {
+            $(this).hide();
+        });
+    });
+
+
